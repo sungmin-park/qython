3e37a0fe6ec13a507238e777090247f0e94e5e62
Park Sung Min <vamf12@gmail.com>
2014-11-12 21:42:35 +0900

    add datetime package

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from sys import argv
 
 from setuptools import setup
 
-version = '0.0.1'
+version = '0.0.2'
 
 install_requires = []
 if 'develop' in argv:
     install_requires += ['pytest>=2.6', 'pytz']
 
-setup(name='qython', version=version, packages=['qython'], license='BSD',
+packages = ['qython', 'qython.datetime']
+
+setup(name='qython', version=version, packages=packages, license='BSD',
       description='A missing library for python.',
       url='https://github.com/vamf12/qython',
       install_requires=install_requires)
